890a080a716e204563ef644fce8a0e69a231ecfc
Federico <118925682+fedeparrilla10@users.noreply.github.com>
2023-01-08 20:14:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
-const book = [
-  {
+const books = {
+  "El Imperio Final": {
     name: "El Imperio Final",
     img: "./img/ImperioFinal.jpg",
     author: "Brandon Sanderson",
     genre: "Fantasia",
     pages: "690",
     details: `Durante mil años han caído las cenizas y nada florece. Durante mil años los skaa han sido esclavizados y viven sumidos en un miedo inevitable. Durante mil años el Lord Legislador reina con un poder absoluto gracias al terror, a sus poderes e inmortalidad.
-      
       Le ayudan «obligadores» e «inquisidores», junto a la poderosa magia de la «alomancia». Pero los nobles, con frecuencia, han tenido trato sexual con jóvenes skaa y, aunque la ley lo prohíbe, algunos de sus bastardos han sobrevivido y heredado los poderes alománticos: son los «nacidos de la bruma» (mistborns).
-      
       Ahora, Kelsier, el «superviviente», el único que ha logrado huir de los Pozos de Hathsin, ha encontrado a Vin, una pobre chica skaa con mucha suerte... Tal vez los dos unidos a la rebelión que los skaa intentan desde hace mil años puedan cambiar el mundo y la atroz dominación del Lord Legislador.`,
+    href: "https://www.goodreads.com/book/show/7183286-el-imperio-final?ref=nav_sb_ss_1_16"
   },
-
-  {
+  "Neverwhere": {
     name: "Neverwhere",
     img: "./img/neverwhere.jpg",
     author: "Neil Gaiman",
     genre: "Fantasy",
     pages: "370",
     details: `Under the streets of London there's a world most people could never even dream of. A city of monsters and saints, murderers and angels, knights in armour and pale girls in black velvet.
-  
       "Neverwhere" is the London of the people who have fallen between the cracks.
-      
       Strange destinies lie in wait in London below - a world that seems eerily familiar. But a world that is utterly bizarre, peopled by unearthly characters such as the Angel called Islington, the girl named Door, and the Earl who holds Court on a tube train.
-      
       Now a single act of kindness has catapulted young businessman Richard Mayhew out of his safe and predictable life - and into the realms of "Neverwhere." Richard is about to find out more than he ever wanted to know about this other London. Which is a pity. Because Richard just wants to go home...`,
+    href: "https://www.goodreads.com/book/show/14497.Neverwhere?ref=nav_sb_ss_1_11"
   },
-
-  {
+  "El Nombre Del Viento": {
     name: "El Nombre del Viento",
     img: "./img/nombreviento.jpg",
     author: "Patrick Rothfuss",
@@ -35,11 +30,13 @@ const book = [
     pages: "872",
     details: `En una posada en tierra de nadie, un hombre se dispone a relatar, por primera vez, la auténtica historia de su vida. Una historia que únicamente él conoce y que ha quedado diluida tras los rumores, las conjeturas y los cuentos de taberna que le han convertido en un personaje legendario a quien todos daban ya por muerto: Kvothe... músico, mendigo, ladrón, estudiante, mago, héroe y asesino.
     Ahora va a revelar la verdad sobre sí mismo. Y para ello debe empezar por el principio: su infancia en una troupe de artistas itinerantes, los años malviviendo como un ladronzuelo en las calles de una gran ciudad y su llegada a una universidad donde esperaba encontrar todas las respuestas que había estado buscando.`,
+    href: "https://www.goodreads.com/book/show/6483211-el-nombre-del-viento?ref=nav_sb_ss_1_20"
   },
-];
+}
 
 function randomPage() {
-  const randomBook = book[Math.floor(Math.random() * book.length)];
+  const keys = Object.keys(books);
+  const randomBook = books[keys[keys.length * Math.random() << 0]];
   const img = document.querySelector(".libro");
   const title = document.querySelector(".booktitle");
   const genre = document.querySelector(".genre");
@@ -52,18 +49,10 @@ function randomPage() {
   img.setAttribute("src", randomBook.img);
 
   img.addEventListener("click", () => {
-    if (title.textContent === "El Imperio Final") {
-      location.href =
-        "https://www.goodreads.com/book/show/7183286-el-imperio-final?ref=nav_sb_ss_1_16";
-    } else if (title.textContent === "Neverwhere") {
-      location.href =
-        "https://www.goodreads.com/book/show/14497.Neverwhere?ref=nav_sb_ss_1_11";
-    } else if (title.textContent === "El Nombre del Viento") {
-      location.href =
-        "https://www.goodreads.com/book/show/6483211-el-nombre-del-viento?ref=nav_sb_ss_1_20";
-    }
+    const { textContent } = title;
+    location.href = books[textContent].href;
   });
-}
+};
 
 const button = document.querySelector("#randomizer");
 button.addEventListener("click", randomPage);
